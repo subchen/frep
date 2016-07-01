6a23e09c20c12f6a90beff099176b6f231730147
Guoqiang Chen <subchen@gmail.com>
2016-07-01 10:56:46 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-ldflags="$ldflags -s -w"
-ldflags="$ldflags -X main.BuildVersion=$BuildVersion"
-ldflags="$ldflags -X main.BuildGitCommit=$GitCommit"
-ldflags="$ldflags -X main.BuildDate=$GitCommit"
+set -e
+
+ldflags="-s -w"
+ldflags="$ldflags -X 'main.BuildVersion=$(git rev-list HEAD --count)'"
+ldflags="$ldflags -X 'main.BuildGitCommit=$(git describe --abbrev=0 --always)'"
+ldflags="$ldflags -X 'main.BuildDate=$(date +%c)'"
 
 mkdir -p dist && GOOS=linux GOARCH=amd64 go build -ldflags "$ldflags" -o dist/frep-linux-amd64
 mkdir -p dist && GOOS=darwin GOARCH=amd64 go build -ldflags "$ldflags" -o dist/frep-darwin-amd64
