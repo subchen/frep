36f547861cd4b5dceed7b5c2de38afa2c7e7e57b
Guoqiang Chen <subchen@gmail.com>
2016-07-19 21:59:49 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,23 +2,32 @@
 
 set -e
 
-DIR=$(cd $(dirname $0); pwd)
-DIST=$DIR/dist
+ROOT=$(cd $(dirname $0); pwd)
+DIST=$ROOT/dist
 
-rm -rf $DIST
+rm -rf $DIST && mkdir -p $DIST
 
 ldflags="-s -w"
 ldflags="$ldflags -X 'main.BuildVersion=$(git rev-list HEAD --count)'"
 ldflags="$ldflags -X 'main.BuildGitCommit=$(git describe --abbrev=0 --always)'"
 ldflags="$ldflags -X 'main.BuildDate=$(date -u -R)'"
 
-mkdir -p $DIST && GOOS=linux GOARCH=amd64 go build -ldflags "$ldflags" -o $DIST/frep-linux-amd64
-mkdir -p $DIST && GOOS=darwin GOARCH=amd64 go build -ldflags "$ldflags" -o $DIST/frep-darwin-amd64
-mkdir -p $DIST && GOOS=windows GOARCH=amd64 go build -ldflags "$ldflags" -o $DIST/frep-windows-amd64.exe
+# build and zip
+echo "building for linux"
+cd $ROOT && GOOS=linux GOARCH=amd64 go build -ldflags "$ldflags" -o $DIST/frep
+cd $DIST && zip -r frep-linux-amd64.zip frep
 
-cd $DIST && zip -r frep-linux-amd64.zip frep-linux-amd64
-cd $DIST && zip -r frep-darwin-amd64.zip frep-darwin-amd64
-cd $DIST && zip -r frep-windows-amd64.zip frep-windows-amd64.exe
+echo "building for darwin"
+cd $ROOT && GOOS=darwin GOARCH=amd64 go build -ldflags "$ldflags" -o $DIST/frep
+cd $DIST && zip -r frep-darwin-amd64.zip frep
 
-cd $DIST && md5sum * > md5sum.txt
+echo "building for windows"
+cd $ROOT && GOOS=windows GOARCH=amd64 go build -ldflags "$ldflags" -o $DIST/frep.exe
+cd $DIST && zip -r frep-windows-amd64.zip frep.exe
+
+# clean
+cd $DIST && rm -rf frep frep.exe
+
+# md5sum
+cd $DIST && md5sum *.zip > md5sum.txt
 
